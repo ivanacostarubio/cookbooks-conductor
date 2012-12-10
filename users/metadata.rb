927bd34cb441c3093b2cf4b7f4d11ca48ffaae37
Ivan Acosta-Rubio <ivan@bakedweb.net>
2012-12-10 11:57:28 -0430

    Adding users

--- users/metadata.rb
+++ users/metadata.rb
@@ -1,4 +1,11 @@
-maintainer        "37signals"
-maintainer_email  "sysadmins@37signals.com"
-description       "Configures users and groups"
-version           "0.1"
+name             "users"
+maintainer       "Opscode, Inc."
+maintainer_email "cookbooks@opscode.com"
+license          "Apache 2.0"
+description      "Creates users from a databag search"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.2.0"
+
+%w{ ubuntu debian redhat centos fedora freebsd}.each do |os|
+  supports os
+end
